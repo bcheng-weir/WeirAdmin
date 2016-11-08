89c85265122b66629d278ce83ca4b8975a0bac91
Dave Ferguson <davfergus@gmail.com>
2016-11-07 20:11:24 -0600

    Added basic objects to the order model for view usage.

--- src/app/order/order.js
+++ src/app/order/order.js
@@ -12,6 +12,7 @@ function OrderShareService() {
     };
     return svc;
 }
+
 function orderConfig($stateProvider, buyerid){
     $stateProvider.state('order', {
         parent: 'base',
@@ -44,87 +45,89 @@ function orderConfig($stateProvider, buyerid){
 }
 
 function OrderController($scope, $state, $sce, OrderCloud, Order, DeliveryAddress, LineItems, Payments, toastr, WeirService){
-        var vm = this;
-        var labels = {
-            en: {
-                //header labels
-                status: "Status",
-                OrderDate: "Order date;",
-                Confirm: "Confirm",
-                Revise: "Revise",
-                ShareRevision: "Share revision",
-                Comments: "Comments",
-                Download: "Download",
-                Print: "Print",
-                //paragraph above table
-                WeirOrderNo: "Weir Order No;",
-                QuoteRef: "Your quote ref;",
-                PONumber: "Your PO No;",
-                //table labels
-                SerialNum: "Serial number",
-                TagNum: "Tag number(if available)",
-                PartNum: "Part Number",
-                Description: "Description of Part",
-                RecReplacement: "Recommended replacement",
-                LeadTimeAvailability: "Lead time / Availability",
-                PricePerItem: "PricePerItem",
-                Quantity: "Quantity",
-                Total: "Total",
-                //labels to right of table
-                Edit: "Edit",
-                Removed: "Removed",
-                Updated: "Updated",
-                New: "New",
-                //buttons above table
-                BackToQuote: "Back to Quotes",
-                AddNewItems: "Add new items",
-                AddABlankItem: "Add a blank item",
-                //footers
-                YourRefNo: "Your Reference No;",
-                DelieveryAddress: "Delievery Address",
-                YourAttachments: "Your attachments",
-                YourComments: "Your comments or instructions"
-},
-            fr: {
-                //header labels
-                status:$sce.trustAsHtml( "Status"),
-                OrderDate:$sce.trustAsHtml( "Order date;"),
-                Confirm:$sce.trustAsHtml( "Confirm"),
-                Revise:$sce.trustAsHtml( "Revise"),
-                ShareRevision:$sce.trustAsHtml( "Share revision"),
-                Comments:$sce.trustAsHtml( "Comments"),
-                Download:$sce.trustAsHtml( "Download"),
-                Print:$sce.trustAsHtml( "Print"),
-                //paragraph above table
-                WeirOrderNo:$sce.trustAsHtml( "Weir Order No;"),
-                QuoteRef:$sce.trustAsHtml( "Your quote ref;"),
-                PONumber:$sce.trustAsHtml( "Your PO No;"),
-                //table labels
-                SerialNum:$sce.trustAsHtml( "Serial number"),
-                TagNum:$sce.trustAsHtml( "Tag number(if available)"),
-                PartNum:$sce.trustAsHtml( "Part Number"),
-                Description:$sce.trustAsHtml( "Description of Part"),
-                RecReplacement:$sce.trustAsHtml( "Recommended replacement"),
-                LeadTimeAvailability:$sce.trustAsHtml( "Lead time / Availability"),
-                PricePerItem:$sce.trustAsHtml( "PricePerItem"),
-                Quantity:$sce.trustAsHtml( "Quantity"),
-                Total:$sce.trustAsHtml( "Total"),
-                //labels to right of table
-                Edit:$sce.trustAsHtml( "Edit"),
-                Removed:$sce.trustAsHtml( "Removed"),
-                Updated:$sce.trustAsHtml( "Updated"),
-                New:$sce.trustAsHtml( "New"),
-                //buttons above table
-                BackToQuote:$sce.trustAsHtml( "Back to Quotes"),
-                AddNewItems:$sce.trustAsHtml( "Add new items"),
-                AddABlankItem:$sce.trustAsHtml( "Add a blank item"),
-                //footers
-                YourRefNo:$sce.trustAsHtml( "Your Reference No;"),
-                DelieveryAddress:$sce.trustAsHtml("Delievery Address"),
-                YourAttachments:$sce.trustAsHtml( "Your attachments"),
-                YourComments:$sce.trustAsHtml( "Your comments or instructions")
-            }
-        };
-        vm.labels = labels[WeirService.Locale()];
-        vm.DeliveryAddress = DeliveryAddress;
+    var vm = this;
+    vm.Order = Order;
+    vm.LineItems = LineItems;
+    vm.DeliveryAddress = DeliveryAddress;
+    var labels = {
+        en: {
+            //header labels
+            status: "Status",
+            OrderDate: "Order date;",
+            Confirm: "Confirm",
+            Revise: "Revise",
+            ShareRevision: "Share revision",
+            Comments: "Comments",
+            Download: "Download",
+            Print: "Print",
+            //paragraph above table
+            WeirOrderNo: "Weir Order No;",
+            QuoteRef: "Your quote ref;",
+            PONumber: "Your PO No;",
+            //table labels
+            SerialNum: "Serial number",
+            TagNum: "Tag number(if available)",
+            PartNum: "Part Number",
+            Description: "Description of Part",
+            RecReplacement: "Recommended replacement",
+            LeadTimeAvailability: "Lead time / Availability",
+            PricePerItem: "PricePerItem",
+            Quantity: "Quantity",
+            Total: "Total",
+            //labels to right of table
+            Edit: "Edit",
+            Removed: "Removed",
+            Updated: "Updated",
+            New: "New",
+            //buttons above table
+            BackToQuote: "Back to Quotes",
+            AddNewItems: "Add new items",
+            AddABlankItem: "Add a blank item",
+            //footers
+            YourRefNo: "Your Reference No;",
+            DelieveryAddress: "Delievery Address",
+            YourAttachments: "Your attachments",
+            YourComments: "Your comments or instructions"
+        },
+        fr: {
+            //header labels
+            status:$sce.trustAsHtml( "Status"),
+            OrderDate:$sce.trustAsHtml( "Order date;"),
+            Confirm:$sce.trustAsHtml( "Confirm"),
+            Revise:$sce.trustAsHtml( "Revise"),
+            ShareRevision:$sce.trustAsHtml( "Share revision"),
+            Comments:$sce.trustAsHtml( "Comments"),
+            Download:$sce.trustAsHtml( "Download"),
+            Print:$sce.trustAsHtml( "Print"),
+            //paragraph above table
+            WeirOrderNo:$sce.trustAsHtml( "Weir Order No;"),
+            QuoteRef:$sce.trustAsHtml( "Your quote ref;"),
+            PONumber:$sce.trustAsHtml( "Your PO No;"),
+            //table labels
+            SerialNum:$sce.trustAsHtml( "Serial number"),
+            TagNum:$sce.trustAsHtml( "Tag number(if available)"),
+            PartNum:$sce.trustAsHtml( "Part Number"),
+            Description:$sce.trustAsHtml( "Description of Part"),
+            RecReplacement:$sce.trustAsHtml( "Recommended replacement"),
+            LeadTimeAvailability:$sce.trustAsHtml( "Lead time / Availability"),
+            PricePerItem:$sce.trustAsHtml( "PricePerItem"),
+            Quantity:$sce.trustAsHtml( "Quantity"),
+            Total:$sce.trustAsHtml( "Total"),
+            //labels to right of table
+            Edit:$sce.trustAsHtml( "Edit"),
+            Removed:$sce.trustAsHtml( "Removed"),
+            Updated:$sce.trustAsHtml( "Updated"),
+            New:$sce.trustAsHtml( "New"),
+            //buttons above table
+            BackToQuote:$sce.trustAsHtml( "Back to Quotes"),
+            AddNewItems:$sce.trustAsHtml( "Add new items"),
+            AddABlankItem:$sce.trustAsHtml( "Add a blank item"),
+            //footers
+            YourRefNo:$sce.trustAsHtml( "Your Reference No;"),
+            DelieveryAddress:$sce.trustAsHtml("Delievery Address"),
+            YourAttachments:$sce.trustAsHtml( "Your attachments"),
+            YourComments:$sce.trustAsHtml( "Your comments or instructions")
+        }
+    };
+    vm.labels = labels[WeirService.Locale()];
 }
