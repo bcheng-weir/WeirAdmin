9c4f82146262258fd373148e66fad5e8ef88688f
bastien1993 <bryce.bastien93@gmail.com>
2016-11-29 12:40:21 -0600

    WeirAdmin User Group Update
    Sets the locale depending on the user weirgroup in Admin app. When
    creating a new admin weir group is required.

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -10,7 +10,31 @@ function HomeConfig($stateProvider) {
 			url: '/home',
 			templateUrl: 'home/templates/home.tpl.html',
 			controller: 'HomeCtrl',
-			controllerAs: 'home'
+			controllerAs: 'home',
+            resolve: {
+                Language: function(OrderCloud, $cookieStore, WeirService) {
+                    OrderCloud.Me.Get().then(function (buyer) {
+                        var lang = WeirService.Locale();
+                        //set the expiration date of the cookie.
+                        var now = new Date();
+                        var exp = new Date(now.getFullYear(), now.getMonth() + 6, now.getDate());
+                        if (buyer.xp.WeirGroup.id == 2 && lang == 'en') {
+                            //make it fr
+                            $cookieStore.put('language', 'fr', {
+                                expires: exp
+                            });
+
+                        }
+                        if (buyer.xp.WeirGroup.id == 1 && lang == 'fr') {
+                            //make it en
+                            $cookieStore.put('language', 'en', {
+                                expires: exp
+                            });
+
+                        }
+                    });
+                }
+            }
 		});
 }
 
