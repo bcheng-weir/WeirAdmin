6ac0eda485e8e4f36d051849d34b2c4d58a3ff06
Ferguson <E054875@mcgladrey.rsm.net>
2017-11-21 13:29:22 -0600

    POC-616: The admin super user can update EN enquiry category names.

---
+++ src/app/enquiries/enquiries.js
@@ -0,0 +1,49 @@
+angular.module('orderCloud')
+    .config(EnquiriesConfig)
+    .controller('EnquiriesCtrl', EnquiriesController);
+
+function EnquiriesConfig($stateProvider) {
+    $stateProvider
+        .state('enquiries', {
+            parent: 'base',
+            templateUrl: 'enquiries/templates/enquiries.tpl.html',
+            controller: 'EnquiriesCtrl',
+            controllerAs: 'enquiries',
+            url: '/EnquiryAdministration',
+            date: { componentName: 'Enquiries' },
+            resolve: {
+                EnquiryCategories: function(OrderCloudSDK) {
+                    var opts = {
+                        'depth': 2,
+                        'filters': {
+                            'ID': '<>Parts',
+                            'ChildCount': '0'
+                        }
+                    };
+
+                    return OrderCloudSDK.Categories.List('WPIFR_ENQ', opts);
+                }
+            }
+        });
+}
+
+function EnquiriesController($exceptionHandler, EnquiryCategories, OrderCloudSDK, toastr) {
+    var vm = this;
+    vm.list = EnquiryCategories.Items;
+
+    vm.Save = function(category) {
+        var opts = {
+            'xp': {
+                'Name': category.xp.Name
+            }
+        };
+
+        OrderCloudSDK.Categories.Patch('WPIFR_ENQ', category.ID, opts)
+            .then(function(category) {
+                toastr.success(category.xp.Name, 'Success');
+            })
+            .catch(function(ex) {
+                $exceptionHandler(ex);
+            });
+    };
+}
