cae4faccc43fef85432feb3553dc22c80936cf99
Ferguson <E054875@mcgladrey.rsm.net>
2018-01-22 20:27:04 -0600

    Correct a missing ; in users.js

--- src/app/users/users.js
+++ src/app/users/users.js
@@ -284,7 +284,7 @@ function UserCreateController($exceptionHandler, $state, toastr, OrderCloudSDK, 
                 });
             })
             .catch(function(ex) {
-                $exceptionHandler(ex)
+                $exceptionHandler(ex);
             });
     };
 }
